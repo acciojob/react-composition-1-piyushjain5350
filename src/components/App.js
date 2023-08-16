1990490437c6a6d17b79b9f1340132d6e2a71dda
Piyush Jain <piyushjain5350@gmail.com>
2023-08-16 11:01:33 +0530

    1 soln

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React , {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [val,setVal]=useState('');
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          <li onClick={()=>setVal("Tab 1")}>Tab 1</li>
+          <li onClick={()=>setVal("Tab 2")}>Tab 2</li>
+          <li onClick={()=>setVal("Tab 3")}>Tab 3</li>
+        </ul>
+
+        <p>This is a content for {val}</p>
     </div>
   )
 }
